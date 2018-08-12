79b211e6c023bdba7021486708ce5027a6202135
Khigashiguchi <econoics0it0bem0116@gmail.com>
2018-08-12 14:59:46 +0900

    Complete Getting started

---
+++ hello.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: higashiguchi0kazuki
+ * Date: 8/12/18
+ * Time: 14:27
+ */
+echo "Hello Hack World!";
---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: higashiguchi0kazuki
+ * Date: 8/12/18
+ * Time: 14:54
+ */
+echo "Hello World";
